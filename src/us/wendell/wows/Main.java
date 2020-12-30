3ef4cd1b2727560174531e1e2de6dc8114abb839
Wending Wu <wending.wu.23@dartmouth.edu>
2020-12-30 16:15:37 -0500

    Add TODOs

--- src/us/wendell/wows/Main.java
+++ src/us/wendell/wows/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //TODO: implement logging or use a package or something
+        //TODO: rest of TODOS in README.md
+        //TODO: update README.md with better description
     }
 }
